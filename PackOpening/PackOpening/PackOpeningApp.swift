477ab098dad189c9551d154c63df666235c997af
Pablo Castro <pablocastrodb@gmail.com>
2024-12-04 11:51:43 +0100

    main: initial commit

---
+++ PackOpening/PackOpening/PackOpeningApp.swift
@@ -0,0 +1,17 @@
+//
+//  PackOpeningApp.swift
+//  PackOpening
+//
+//  Created by Pablo Castro on 4/12/24.
+//
+
+import SwiftUI
+
+@main
+struct PackOpeningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
